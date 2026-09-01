ba02f0b6eb1896f70f9687e0ce77ccd8cc14b730
shriyabhavanasi12-cell <shriyabhavanasi.12@gmail.com>
2026-09-01 13:06:29 +0530

    change the paint color

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function setPixelColour(pixel){
-    pixel.style.backgroundColor="black";
+    pixel.style.backgroundColor="red";
 
 }
+document.addEventListener("DOMContentLoaded", ()=>{
+
+const pixels=document.querySelectorAll(".pixel");
+pixels.forEach((pixel)=>{
+
+pixel.addEventListener("click",()=>setPixelColour(pixel));
+
+});
+
+
+});
 
